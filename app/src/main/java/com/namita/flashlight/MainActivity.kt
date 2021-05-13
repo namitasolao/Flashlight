709376e11e97eddc95ffb726cf2ceca2b13816bf
Namita Solao <namita.solao@gmail.com>
2021-05-12 17:30:18 -0700

    changed the package name

--- app/src/main/java/com/namita/flashlight/MainActivity.kt
+++ app/src/main/java/com/namita/flashlight/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.example.flashlight1
+package com.namita.flashlight
 
 import android.content.Context
-import android.content.Context.*
 import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.hardware.camera2.CameraManager
 import android.media.MediaPlayer
-import android.media.Ringtone
 import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
@@ -35,14 +32,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun buttonClicked(myButton : Switch){
+    private fun buttonClicked(myButton: Switch) {
 
         val isFlashAvailable = applicationContext.packageManager
             .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)
         if (!isFlashAvailable) {
             flashNotAvail()
-        }
-        else {
+        } else {
             cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
             cameraId = cameraManager.cameraIdList[0]
             cameraManager.setTorchMode(cameraId, myButton.isChecked)
@@ -60,32 +56,28 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun buttonFormat(myButton : Switch){
-        if(myButton.isChecked){
+    private fun buttonFormat(myButton: Switch) {
+        if (myButton.isChecked) {
             //myButton.setBackgroundColor(R.color.black)
-            Toast.makeText(this,"Light is turned on",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Light is turned on", Toast.LENGTH_SHORT).show()
             findViewById<ImageView>(R.id.lightOn_image).isVisible = false
             findViewById<ImageView>(R.id.lightOff_image).isVisible = true
 
 
-        }
-        else {
-           // myButton.setBackgroundColor(R.color.teal_700)
-            Toast.makeText(this,"Light is turned off",Toast.LENGTH_SHORT).show()
+        } else {
+            // myButton.setBackgroundColor(R.color.teal_700)
+            Toast.makeText(this, "Light is turned off", Toast.LENGTH_SHORT).show()
             findViewById<ImageView>(R.id.lightOn_image).isVisible = true
             findViewById<ImageView>(R.id.lightOff_image).isVisible = false
 
         }
+    }
+
+    private fun soundEffect() {
+
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val mp = MediaPlayer.create(applicationContext, alarmSound)
+        mp.start()
 
     }
-private fun soundEffect() {
-
-    val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-    val mp = MediaPlayer.create(applicationContext, alarmSound)
-    mp.start()
-
-//    var mp = MediaPlayer.create(this, R.raw.beep23)
-//    mp.start()
-
-}
 }
